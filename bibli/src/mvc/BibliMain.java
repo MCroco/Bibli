cc3049406bb64d8af8e3d2aecb7cc0a7316cc9a7
MCroco <miricaconstantin@gmail.com>
2020-07-17 01:12:24 +0200

    first class + structure mvc

---
+++ bibli/src/mvc/BibliMain.java
@@ -0,0 +1,15 @@
+package mvc;
+
+
+//import mvc.controller.*;
+//import mvc.model.*;
+//import mvc.view.*;
+
+public class BibliMain {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
