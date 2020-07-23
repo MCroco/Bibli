22add3c56f2831edf1791682d6b9794c5da32cb5
MCroco <miricaconstantin@gmail.com>
2020-07-23 21:02:46 +0200

    Create the base controller - still to be linked with the rest that must be done

---
+++ bibli/src/farm/controller/TheController.java
@@ -0,0 +1,19 @@
+package farm.controller;
+
+
+import farm.model.TheModel;
+import farm.view.TheView;
+
+/**
+ *
+ * @author MConstantin
+ */
+public class TheController {
+    
+    public TheController(TheModel model, TheView view) {
+       //a faire
+
+    }
+
+
+}
