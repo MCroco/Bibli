8a20b1f44517546b17f4c6a66b6733619eeab718
MCroco <miricaconstantin@gmail.com>
2020-07-23 21:11:14 +0200

    Create a model

---
+++ bibli/src/farm/model/TheModel.java
@@ -0,0 +1,24 @@
+/**
+ * Class The Model
+ * 
+ * Purpose:
+ * 
+ * Responsible for creating the instances of the setUp and search classes 
+ * 
+ * In this class we have methods for serialization and deserialization which will be the methods responsible for storing the data and getting it back  
+ * 
+ */
+package farm.model;
+
+
+/**
+ *
+ * @author MConstantin
+ */
+public class TheModel  {
+
+	public TheModel() {
+
+	}
+
+}
