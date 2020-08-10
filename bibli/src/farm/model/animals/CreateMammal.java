355ac5c9061c630dab21725e74cd751ea0c78623
MCroco <miricaconstantin@gmail.com>
2020-08-11 00:55:21 +0200

    create mammal that might have an offspring

---
+++ bibli/src/farm/model/animals/CreateMammal.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farm.model.animals;
+
+import java.util.ArrayList;
+
+
+
+/**
+ *
+ * @author MConstantin
+ */
+public class CreateMammal extends Animal implements Mammal {
+    
+    private int furry; 
+    
+    public CreateMammal(String specie, String name, String dateOfBirth, String dateOfArrival, int gender, Medication medication, Vaccine vaccine) {
+        super(specie, name, dateOfBirth, dateOfArrival, gender, medication, vaccine);
+    
+    }
+    
+    public int getFurry() {
+        return furry;
+    }
+
+    public void setFurry(int furry) {
+        this.furry = furry;
+    }
+    
+    public String toString() {
+        String offpring = "";
+        if (getOffsprings().size() != 0){
+            for (int i = 0 ; i < getOffsprings().size() ; i++){
+                if (i == getOffsprings().size() - 1){
+                    offpring += String.valueOf(getOffsprings().get(i).getExhibitionNumb());
+                }else{
+                    offpring += String.valueOf(getOffsprings().get(i).getExhibitionNumb())+", ";
+                }
+            }
+            offpring = "Animal: "+offpring;
+        }else{
+            offpring = "No offprings";
+        }
+    return super.toString()
+           +"\tFurry:           «"+getFurry()+"»\n"
+           +"\tOffprings:                       \n"
+                +"\t\t"+offpring+              "\n";
+    }
+
+    @Override
+    public void createOffpring(String name, String dateOfBirth, String dateOfArrival, int gender, Medication medication, Vaccine vaccine) {
+        getOffsprings().add(new CreateMammal(this.getSpecie(), name, dateOfBirth, dateOfArrival, gender, medication, vaccine));
+     
+    }
+    
+    @Override
+    public ArrayList<String> toArrayList(){
+        ArrayList<String> list = super.toArrayList();
+        list.add(hasFur(furry));
+        return list;
+    }
+    
+    @Override
+    public ArrayList<String> nameVariables(){
+        ArrayList<String> names = super.nameVariables();
+        names.add("furry");
+        return names;
+    }
+
+
+    
+}
