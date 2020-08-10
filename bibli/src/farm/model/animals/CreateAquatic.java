2f95b8c0db5c03c2a6f56d00168976cd30eb148e
MCroco <miricaconstantin@gmail.com>
2020-08-11 00:49:38 +0200

    create an interface for the avian animals

---
+++ bibli/src/farm/model/animals/CreateAquatic.java
@@ -0,0 +1,41 @@
+package farm.model.animals;
+
+import java.util.ArrayList;
+
+public class CreateAquatic extends Animal implements Aquatic {
+    
+    private int canBeOutSideWatter;
+    
+    public CreateAquatic(String specie, String name, String dateOfBirth, String dateOfArrival, int gender, Medication medication, Vaccine vaccine) {
+        super(specie, name, dateOfBirth, dateOfArrival, gender, medication, vaccine);
+        
+    }
+    
+    @Override
+    public void createOffpring(String name, String dateOfBirth, String dateOfArrival, int gender, Medication medication, Vaccine vaccine) {
+        getOffsprings().add(new CreateAquatic(this.getSpecie(), name, dateOfBirth, dateOfArrival, gender, medication, vaccine));
+    }
+
+    public void setCanBeOutSideWatter(int canBeOutSideWatter) {
+        this.canBeOutSideWatter = canBeOutSideWatter;
+    }    
+    
+    public int getCanBeOutSideWatter() {
+        return canBeOutSideWatter;
+    }
+    
+    @Override
+    public ArrayList<String> toArrayList(){
+        ArrayList<String> list = super.toArrayList();
+        list.add(canBeOutsideWatter(canBeOutSideWatter));
+        return list;
+    }
+    
+    @Override
+    public ArrayList<String> nameVariables(){
+        ArrayList<String> names = super.nameVariables();
+        names.add("canBeOutSideWatter");
+        return names;
+    }
+    
+}
