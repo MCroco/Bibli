60f5a5a4bde896d9d7751052c1aa3e1c79cb5994
MCroco <miricaconstantin@gmail.com>
2020-08-12 22:33:32 +0200

    creating some default classes for the view

--- bibli/src/farm/view/TheView.java
+++ bibli/src/farm/view/TheView.java
@@ -1,12 +1,80 @@
 package farm.view;
 
+import java.util.ArrayList;
+
 import farm.model.TheModel;
+import farm.model.animals.Animal;
+
+
+/**
+ * creating already some kind of structure
+ */
+import farm.view.animals.AddAnimalFrame;
+import farm.view.animals.UpdateAnimalFrame;
+import farm.view.animals.ViewAnimalsFrame;
+
 
 /**
  *
  * @author MConstantin
  */
 public class TheView {
+
+    ViewAnimalsFrame viewAnimals;
+    UpdateAnimalFrame updateAnimal;
+    Dashboard adminDash;
+    AddAnimalFrame addAnimal;
+
+
+    ArrayList<Animal> animals;
+
+
+    public TheView(TheModel model) {
+
+        this.animals = model.getListAnimals();
+
+
+        this.adminDash = new Dashboard(this, "Fenetre principale", 340, 300, "src/farm/view/images/animal.png");
+
+    }
+
+    public void setAddAnimal(AddAnimalFrame addAnimal) {
+        this.addAnimal = addAnimal;
+    }
+
+
+
+    public AddAnimalFrame getAddAnimal() {
+        return addAnimal;
+    }
+
+    public ViewAnimalsFrame getViewAnimals() {
+        return viewAnimals;
+    }
+
+    public void setViewAnimals(ViewAnimalsFrame viewAnimals) {
+        this.viewAnimals = viewAnimals;
+    }
+
+    public void setUpdateAnimal(UpdateAnimalFrame updateAnimal) {
+        this.updateAnimal = updateAnimal;
+    }
+
+    public UpdateAnimalFrame getUpdateAnimal() {
+        return updateAnimal;
+    }
+
+    public Dashboard getAdminDash() {
+        return adminDash;
+    }
+
+    public ArrayList<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAdminDash(Dashboard adminDash) {
+        this.adminDash = adminDash;
+    }
 
 
 }
